0af354418baf4a5ef044037830fd351855a5a00f
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-06-15 16:49:44 +0200

    fixed some margins so there's more whitespace

--- src/components/products/SingleProduct.jsx
+++ src/components/products/SingleProduct.jsx
@@ -5,15 +5,15 @@ const SingleProduct = () => {
     //product image(s)
     <Flex direction={['column','column','row']} align='center' gap={{md:'12',lg:'16'}} marginY={{md:'6'}}>
         <Flex direction="column" gap={6}>
-            <Image borderRadius={{md:'6px'}} src="../images/products/image-product-1.jpg"/>
-            <SimpleGrid columns="4" spacing={6}>
+            <Image  borderRadius={{md:'6px'}} src="../images/products/image-product-1.jpg"/>
+            <SimpleGrid  columns="4" spacing={6} display={['none','none','grid']}>
               <Image  borderRadius={{md:'6px'}} src="../images/products/image-product-1-thumbnail.jpg"/>
               <Image  borderRadius={{md:'6px'}} src="../images/products/image-product-2-thumbnail.jpg"/>
               <Image borderRadius={{md:'6px'}} src="../images/products/image-product-3-thumbnail.jpg"/>
               <Image  borderRadius={{md:'6px'}} src="../images/products/image-product-4-thumbnail.jpg"/>
             </SimpleGrid>
         </Flex>
-        <VStack align='left'>
+        <VStack  marginBottom={'6'} marginTop={['6','6','0']} align='left'>
            <Heading color='primary.orange' textTransform='uppercase' size='sm'>Sneaker Company</Heading>
            <Heading>Fall Limited Edition Sneakers</Heading>
            <Text paddingY={{md:'6'}} color='neutral.dark'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</Text>
@@ -24,11 +24,11 @@ const SingleProduct = () => {
             </Flex>
             <Text fontWeight='bold' color='neutral.grayish' textDecoration='line-through'>$250.00</Text>
           </Flex>
-          <SimpleGrid  columns={['1','1','2']} gap={['2','2','6']}>
+          <SimpleGrid  columns={['1','1','2']} gap={['2','2','6']} >
             <Flex  borderRadius='4px' background='neutral.light' p={4} align='center' justify='space-between'>
-              <Image src='../images/icons/icon-minus.svg'/>
+              <Image cursor='pointer' src='../images/icons/icon-minus.svg'/>
               <Text>3</Text>
-              <Image src='../images/icons/icon-plus.svg'/>
+              <Image  cursor='pointer' src='../images/icons/icon-plus.svg'/>
             </Flex>
             <Button py={7} backgroundColor='primary.orange' color='white' gap={4}>
               <Image src='../images/icons/icon-cart--white.svg' color='white'/>
