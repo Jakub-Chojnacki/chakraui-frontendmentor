ee8788ec3e789b1b59e2c6544f341cdba3176323
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-06-15 13:58:57 +0200

    singleProduct works on desktop

--- src/components/products/SingleProduct.jsx
+++ src/components/products/SingleProduct.jsx
@@ -3,32 +3,40 @@ import { SimpleGrid,VStack,Flex,Image,Heading,Text,Box,Button} from '@chakra-ui/
 const SingleProduct = () => {
   return (
     //product image(s)
-    <SimpleGrid columns={1} spacing={10}>
-        <Flex>
-            <Image src="../images/products/image-product-1.jpg"/>
+    <Flex direction={['column','column','row']} align='center' gap={{md:'8'}} marginY={{md:'6'}}>
+        <Flex direction="column" gap={6}>
+            <Image borderRadius={{md:'6px'}} src="../images/products/image-product-1.jpg"/>
+            <SimpleGrid columns="4">
+              <Image boxSize='80px' borderRadius={{md:'6px'}} src="../images/products/image-product-1-thumbnail.jpg"/>
+              <Image boxSize='80px' borderRadius={{md:'6px'}} src="../images/products/image-product-2-thumbnail.jpg"/>
+              <Image boxSize='80px' borderRadius={{md:'6px'}} src="../images/products/image-product-3-thumbnail.jpg"/>
+              <Image boxSize='80px' borderRadius={{md:'6px'}} src="../images/products/image-product-4-thumbnail.jpg"/>
+            </SimpleGrid>
         </Flex>
         <VStack align='left'>
-            <Heading color='primary.orange' textTransform='uppercase' size='sm'>Sneaker Company</Heading>
-            <Heading>Fall Limited Edition Sneakers</Heading>
-            <Text color='neutral.dark'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</Text>
-        </VStack>
-        <Flex direction={{md:'column'}} justify='space-between'>
-          <Flex align='center' gap={6}>
-            <Text fontSize='3xl' fontWeight='bold'>$125.00</Text>
-            <Box py={1} px={3} borderRadius='4px' backgroundColor='primary.pale' color='primary.orange' fontWeight='bold'>50%</Box>
+           <Heading color='primary.orange' textTransform='uppercase' size='sm'>Sneaker Company</Heading>
+           <Heading>Fall Limited Edition Sneakers</Heading>
+           <Text paddingY={{md:'6'}} color='neutral.dark'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</Text>
+          <Flex direction={{md:'column'}} justify='space-between' paddingBottom={8}>
+            <Flex align='center' gap={6}>
+              <Text fontSize='3xl' fontWeight='bold'>$125.00</Text>
+              <Box py={1} px={3} borderRadius='4px' backgroundColor='primary.pale' color='primary.orange' fontWeight='bold'>50%</Box>
+            </Flex>
+            <Text fontWeight='bold' color='neutral.grayish' textDecoration='line-through'>$250.00</Text>
           </Flex>
-          <Text fontWeight='bold' color='neutral.grayish' textDecoration='line-through'>$250.00</Text>
-        </Flex>
-        <Flex  borderRadius='4px' background='neutral.light' p={4} align='center' justify='space-between'>
-          <Image src='../images/icons/icon-minus.svg'/>
-          <Text>3</Text>
-          <Image src='../images/icons/icon-plus.svg'/>
-        </Flex>
-        <Button py={7} backgroundColor='primary.orange' color='white' gap={4}>
-          <Image src='../images/icons/icon-cart--white.svg' color='white'/>
-          Add to cart
-          </Button>
-    </SimpleGrid>
+          <SimpleGrid  columns={['1','1','2']} gap={['2','2','6']}>
+            <Flex  borderRadius='4px' background='neutral.light' p={4} align='center' justify='space-between'>
+              <Image src='../images/icons/icon-minus.svg'/>
+              <Text>3</Text>
+              <Image src='../images/icons/icon-plus.svg'/>
+            </Flex>
+            <Button py={7} backgroundColor='primary.orange' color='white' gap={4}>
+              <Image src='../images/icons/icon-cart--white.svg' color='white'/>
+              Add to cart
+              </Button>
+          </SimpleGrid> 
+        </VStack>
+    </Flex>
     //product description
   )
 }
